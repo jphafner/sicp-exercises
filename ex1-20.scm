05eeec8430b5a8c2e1e90b938e106e4b6631068f
Jeffrey Hafner <jphafner@buffalo.edu>
2016-08-30 19:26:59 -0400

    started ex1-20.scm

---
+++ ex1-20.scm
@@ -0,0 +1,13 @@
+
+
+;; NOTE logarithmic growth
+(define (gcd a b)
+  (if (= b 0)
+    a
+    (gcd b (remainder a b))))
+
+;; Lame's Theorem
+;; if Euclid's Algorithm requires $k$ steps to computer the GCD of some pair,
+;; then the smaller numbe rin the pair must be greater than or equal tot he $k^{th}$ Fibonacci number
+
+
