8c6ded5846caf0967122fe6bcf3f79b556178306
Jeffrey Hafner <jphafner@buffalo.edu>
2016-08-30 19:34:51 -0400

    updated ex1-20.scm

--- ex1-20.scm
+++ ex1-20.scm
@@ -11,3 +11,22 @@
 ;; then the smaller numbe rin the pair must be greater than or equal tot he $k^{th}$ Fibonacci number
 
 
+;(gcd 206 40)
+;$1 = 2
+
+
+;; normal-order
+(gcd 206 40)
+(if (= 40 0) 206 (gcd 40 (remainder 206 40)))
+(gcd 40 (remainder 206 40))
+(if (= (remainder 206 40) 0) a (gcd 40 (remainder 40 (remainder 206 40))))
+
+
+;; applicative-order
+(gcd 206 40)
+(if (= 40 0) 206 (gcd 40 (remainder 206 40)))
+(if (= 40 0) 206 (if (= b 0) a (gcd b (remainder a b))))
+
+(define (gcd a b) (if (= b 0) a (gcd b (remainder a b))))
+
+
