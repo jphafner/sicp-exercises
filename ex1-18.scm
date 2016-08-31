19d9f23276007fe36227f01e4c8fd1d33e871daa
Jeffrey Hafner <jphafner@buffalo.edu>
2016-08-31 12:44:12 -0400

    modified ex1-18.scm

--- ex1-18.scm
+++ ex1-18.scm
@@ -8,6 +8,7 @@
 
 ;; wikipedia ancient egyptian multiplication
 (define (mult a b)
+  ;; add some logic to halve smaller of two numbers??
   (cond ((= a 0) 0)
         ((= a 1) b)
         (else (mult (halve a) (double b)))))
